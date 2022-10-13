b5efa506ddab233fd4828ec973b77d3502ce1dfa
neverorfrog <97flavio.maiorana@gmail.com>
2022-10-13 18:30:43 +0200

    Started the second exercise on linear algebra and binary search tree

---
+++ 02-cpp_intro/exercise/src/btree/btree.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+struct TreeNodeInt {
+  int value;
+  TreeNodeInt* left; //this is a pointer
+  TreeNodeInt* right; //this is a pointer
+  TreeNodeInt(int value=0,
+              TreeNodeInt* left=0,
+              TreeNodeInt* right=0);
+
+  ~TreeNodeInt();
+
+  TreeNodeInt* find(int value);
+  
+  bool add(int value);
+};
+
+std::ostream& operator << (std::ostream& os, const TreeNodeInt& node);
+
+  
